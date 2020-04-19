bd71174fb14f04669c9a336381aaab57fd8060ed
Maria Kwiatkowska <maria.kwiatkowska@travelist.pl>
2020-04-19 15:53:08 +0200

    pierwsza pd

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './styles/style.sass';
+import defaultExport from './script';
+
+defaultExport();
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  'style-loader',
+                  'css-loader',
+                  'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[contenthash].[ext]',
+                    }
+                  },
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
+}
